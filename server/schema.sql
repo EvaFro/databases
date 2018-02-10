698a90f21bf8c9e341835634854caf82fe30a864
EvaFro <frolli.erin@gmail.com>
2018-02-09 19:44:39 -0800

    Edited the corect sintax to run in the sql terminal

--- server/schema.sql
+++ server/schema.sql
@@ -1,6 +1,6 @@
-CREATE DATABASE chat3;
+CREATE DATABASE chat12;
 
-USE chat;
+USE chat12;
 
 -- CREATE TABLE messages (
 --    Describe your table here.
@@ -27,87 +27,64 @@ USE chat;
 -- SET FOREIGN_KEY_CHECKS=0;
 
 -- ---
--- Table 'messages'
+-- Table messages
 -- Contains the message objects in table form.
 -- ---
 
-/*DROP TABLE IF EXISTS `messages`;*/
     
-CREATE TABLE 'messages' (
-  'id' INTEGER NULL AUTO_INCREMENT DEFAULT NULL,
-  'username' VARCHAR NULL DEFAULT NULL,
-  'roomname' VARCHAR NULL DEFAULT NULL,
-  'createdAt' TIMESTAMP NULL DEFAULT NULL,
-  'changedAt' TIMESTAMP NULL DEFAULT NULL,
-  'text' VARCHAR NULL DEFAULT NULL,
-  PRIMARY KEY ('id')
-) COMMENT 'Contains the message objects in table form.';
+CREATE TABLE messages (
+  id INTEGER(11) PRIMARY KEY,
+  username VARCHAR(255),
+  roomname VARCHAR(255),
+  createdAt VARCHAR(255),
+  changedAt VARCHAR(255),
+  text VARCHAR(255)
+);
 
 -- ---
--- Table 'users'
--- This the table for assigning id''s to each user.
+-- Table users
+-- This the table for assigning ids to each user.
 -- ---
 
-DROP TABLE IF EXISTS 'users';
     
-CREATE TABLE 'users' (
-  'id' INTEGER NULL AUTO_INCREMENT DEFAULT NULL,
-  'username' VARCHAR NULL DEFAULT NULL,
-  PRIMARY KEY ('id', 'username')
-) COMMENT 'This the table for assigning id''s to each user.';
+CREATE TABLE users (
+  id INTEGER(11) PRIMARY KEY,
+  username VARCHAR(255)
+); 
 
 -- ---
--- Table 'rooms'
--- This the table for assigning id''s to each room.
+-- Table rooms
+-- This the table for assigning ids to each room.
 -- ---
 
-DROP TABLE IF EXISTS 'rooms';
     
-CREATE TABLE 'rooms' (
-  'id' INTEGER NULL AUTO_INCREMENT DEFAULT NULL,
-  'roomnames' VARCHAR NULL DEFAULT NULL,
-  PRIMARY KEY ('id', 'roomnames')
-) COMMENT 'This the table for assigning id''s to each room.';
-
--- ---
--- Table 'date'
--- This is the table for assigning id''s to each timestamp.
--- ---
-
-DROP TABLE IF EXISTS 'date';
-    
-CREATE TABLE 'date' (
-  'id' INTEGER NULL AUTO_INCREMENT DEFAULT NULL,
-  'createdAt' TIMESTAMP NULL DEFAULT NULL,
-  PRIMARY KEY ('id', 'createdAt')
-) COMMENT 'This is the table for assigning id''s to each timestamp.';
-
+CREATE TABLE rooms (
+  id INTEGER(11) PRIMARY KEY,
+  roomnames VARCHAR(255)
+); 
 -- ---
 -- Foreign Keys 
 -- ---
 
-ALTER TABLE 'messages' ADD FOREIGN KEY (username) REFERENCES 'users' ('id');
-ALTER TABLE 'messages' ADD FOREIGN KEY (roomname) REFERENCES 'rooms' ('id');
-ALTER TABLE 'messages' ADD FOREIGN KEY (createdAt) REFERENCES 'date' ('id');
+-- ALTER TABLE messages ADD  (username) REFERENCES users (id);
+-- ALTER TABLE messages ADD FOREIGN KEY (roomname) REFERENCES rooms (id);
 
 -- ---
 -- Table Properties
 -- ---
 
--- ALTER TABLE `messages` ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
--- ALTER TABLE `users` ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
--- ALTER TABLE `rooms` ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
--- ALTER TABLE `date` ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+-- ALTER TABLE messages ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+-- ALTER TABLE users ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+-- ALTER TABLE rooms ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 -- ---
 -- Test Data
 -- ---
 
--- INSERT INTO `messages` (`id`,`username`,`roomname`,`createdAt`,`changedAt`,`text`) VALUES
--- ('','','','','','');
--- INSERT INTO `users` (`id`,`username`) VALUES
--- ('','');
--- INSERT INTO `rooms` (`id`,`roomnames`) VALUES
--- ('','');
--- INSERT INTO `date` (`id`,`createdAt`) VALUES
--- ('','');
+-- INSERT INTO messages (id,username,roomname,createdAt,changedAt,text) VALUES
+-- (,,,,,);
+-- INSERT INTO users (id,username) VALUES
+-- (,);
+-- INSERT INTO rooms (id,roomnames) VALUES
+-- (,);
+-- (,);
